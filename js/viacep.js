2fdc06306eccb5ab2b593f562e80c7d5a4ce46c3
DWOVÂNNA SANTOS NASCIMENTO <22282185@CFP127ALUNO.LOCAL>
2023-03-16 16:55:29 -0300

    first commit

---
+++ js/viacep.js
@@ -0,0 +1,16 @@
+'use scrit'
+
+export const pesquisarCep = async(cepPesquisa) => {
+    const url = `https://viacep.com.br/ws/${cepPesquisa}/json/`
+    const response = await fetch(url)
+    const data = await response.json()
+
+
+    return{
+        logradouro: data.logradouro,
+        bairro: data.bairro,
+        municipio: data.logradouro,
+        estado: data.uf
+    }
+
+}
